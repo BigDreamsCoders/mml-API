3b6148fdfcd864f048761788c583fe66d7950a62
TheAlexBig <00062816@uca.edu.sv>
2019-11-18 15:34:56 -0600

    Some unit test added

---
+++ src/test/kotlin/com/music/feed/musician/MusicianTest.kt
@@ -0,0 +1,22 @@
+package com.music.feed.musician
+
+import com.music.feed.BaseTest
+import org.junit.Assert
+import org.junit.Test
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+
+class MusicianTest : BaseTest() {
+    @Test
+    fun getAllMusicians(){
+        val uri = "musician/all"
+        val mvcResult: MvcResult = mockMvc.perform(MockMvcRequestBuilders.get(base + uri)
+                .accept(MediaType.APPLICATION_JSON)).andReturn()
+        val status = mvcResult.response.status
+        val content = mvcResult.response.contentAsString
+        Assert.assertEquals(200, status)
+        Assert.assertNotNull(content)
+    }
+
+}
---
+++ src/test/kotlin/com/music/feed/song/SongTest.kt
@@ -0,0 +1,21 @@
+package com.music.feed.song
+
+import com.music.feed.BaseTest
+import org.junit.Assert
+import org.junit.Test
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+
+class SongTest : BaseTest() {
+    @Test
+    fun getAllSongs(){
+        val uri = "song/all"
+        val mvcResult: MvcResult = mockMvc.perform(MockMvcRequestBuilders.get(base + uri)
+                .accept(MediaType.APPLICATION_JSON)).andReturn()
+        val status = mvcResult.response.status
+        val content = mvcResult.response.contentAsString
+        Assert.assertEquals(200, status)
+        Assert.assertNotNull(content)
+    }
+}
